9a7feb652096810ae7750fb6533a5f25e7a7b1e9
Yura Ravinsky <ravinskyyuriy@gmail.com>
2018-11-21 12:34:53 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+How to see the app.
+
+1.Clone this repo.
+2.Run npm install from the root.
+3.Run npm dev to start app at localhost:3000.
--- webpack.client.js
+++ webpack.client.js
@@ -4,6 +4,7 @@ const baseConfig = require('./webpack.base.js');
 
 const config = {
   entry: './src/client/client.js',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
